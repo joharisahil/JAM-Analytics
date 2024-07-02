6d462080dfd16d82f5c37db0f3b28b3a8435ba0c
JohariSahil <sahiljohari04@gmail.com>
2024-07-02 14:20:36 +0530

    added colour for JAM analytics

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,46 +1,42 @@
 import React from "react";
-import image from '../../assets/images/object-1.png'
+import image from "../../assets/images/object-1.png";
 import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
-      <div className="aboutus-container" id="about">
-          <div className="aboutus-wrapper">
-              <div className="aboutus-left-content">
-                  <h1 className="aboutus-title">ABOUT COMPANY</h1>
-                  <p className="aboutus-paragraph-first">
-                      At Cynthronix, we are a leading IT solutions provider
-                      dedicated to delivering innovative and cutting-edge
-                      technology services.
-                  </p>
-                  <p className="aboutus-paragraph-second">
-                      With a team of skilled professionals and a passion for
-                      excellence, we strive to empower businesses with advanced
-                      digital solutions that drive growth, efficiency, and
-                      success. With a strong commitment to customer
-                      satisfaction, we work closely with our clients to
-                      understand their unique needs and deliver tailored IT
-                      solutions that exceed expectations. Trust us to be your
-                      reliable partner in navigating the ever-evolving
-                      technology landscape and unlocking the full potential of
-                      your business.
-                  </p>
-                  <button className="aboutus-button">
-                      <a href="#contact">Reach Us</a>
-                  </button>
-              </div>
-              <div className="aboutus-right-content">
-                  <div className="image_wrapper">
-                      <img
-                          src={image}
-                          alt="cynthronix"
-                          srcset=""
-                          className="image"
-                      />
-                  </div>
-              </div>
+    <div className="aboutus-container" id="about">
+      <div className="aboutus-wrapper">
+        <div className="aboutus-left-content">
+          <h1 className="aboutus-title">ABOUT COMPANY</h1>
+          <p className="aboutus-paragraph-first">
+            <span className="aboutus-paragraph-company">
+              JAM Analytics Pvt. Ltd.
+            </span>{" "}
+            pioneers advanced ERP and LMS solutions tailored for educational
+            institutions and corporate entities.
+          </p>
+          <p className="aboutus-paragraph-second">
+            With a commitment to innovation, we specialize in enhancing
+            operational efficiency and educational outcomes through cutting-edge
+            technology. Our comprehensive suite of services integrates
+            seamlessly, providing holistic management solutions that empower
+            clients to streamline processes and achieve strategic goals
+            effortlessly. Backed by a dedicated team of experts, we ensure
+            unparalleled support and customization to meet the unique needs of
+            each client. Discover how JAM Analytics transforms education and
+            business management with precision and reliability.
+          </p>
+          <button className="aboutus-button">
+            <a href="#contact">Reach Us</a>
+          </button>
+        </div>
+        <div className="aboutus-right-content">
+          <div className="image_wrapper">
+            <img src={image} alt="cynthronix" srcset="" className="image" />
           </div>
+        </div>
       </div>
+    </div>
   );
 };
 
